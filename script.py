827fc805adaf90f49afd29f0c114004f4f6fbdcc
Daniel Baigel <danielbaigel@Daniels-MacBook-Pro.local>
2018-11-11 15:46:54 +0800

    fox, wash post, nbc scraping setup

--- script.py
+++ script.py
@@ -44,26 +44,38 @@ FOXcontainer = soupFOX.find(class_="collection collection-spotlight has-hero")
 FOXcontainer2 = FOXcontainer.find('header', class_='info-header')
 
 FOXheadline = FOXcontainer2.find('a').get_text()
-
+print(FOXheadline)
 print("Done with FOX")
+
 ##########################NBC#################################################
-# urlNBC = requests.get("https://www.nbcnews.com/")
-# if urlNBC.status_code != 200:
-#     print(urlNBC.status_code + "\n")
-#     print("Something is wrong with NBC...")
-#     sys.exit("Check website status code")
+urlNBC = requests.get("https://www.nbcnews.com/")
+if urlNBC.status_code != 200:
+    print(urlNBC.status_code + "\n")
+    print("Something is wrong with NBC...")
+    sys.exit("Check website status code")
 
-# soupNBC = BeautifulSoup(urlNBC.content, 'html.parser')
-# print("Done with NBC")
+soupNBC = BeautifulSoup(urlNBC.content, 'html.parser')
+
+NBCcontainer = soupNBC.find('article', class_="teaseCard content___3FGvZ")
+NBCcontainer2 = NBCcontainer.find('h2', class_="teaseCard__headline title___1T2jw title___1Fy4v")
+
+NBCheadline = NBCcontainer2.find('a').get_text()
+print(NBCheadline)
+print("Done with NBC")
+
 # ##########################WP#################################################
-# urlWP = requests.get("https://www.washingtonpost.com/?noredirect=on")
-# if urlWP.status_code != 200:
-#     print(urlWP.status_code + "\n")
-#     print("Something is wrong with Washington Post...")
-#     sys.exit("Check website status code")
+urlWP = requests.get("https://www.washingtonpost.com/?noredirect=on")
+if urlWP.status_code != 200:
+    print(urlWP.status_code + "\n")
+    print("Something is wrong with Washington Post...")
+    sys.exit("Check website status code")
 
-# soupWP = BeautifulSoup(urlWP.content, 'html.parser')
-# print("Done with WP")
+soupWP = BeautifulSoup(urlWP.content, 'html.parser')
+
+WPcontainer = soupWP.find(class_="headline normal normal-style text-align-inherit ")
+WPheadline = WPcontainer.find('a').get_text()
+print(WPheadline)
+print("Done with WP")
 
 ####################################################################################
 
